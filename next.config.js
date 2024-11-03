1a13ee4be17f3441ffb495966482a258b165fe34
Felipe Gerhardt <flc.gerhardt@gmail.com>
2024-11-03 18:00:34 -0500

    next config changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+/**
+ * @type {import('next').NextConfig}
+ */
+ const nextConfig = {
+  output: 'export',
+  basePath: '/fernanda-landing-page'
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 }
-
+ 
 module.exports = nextConfig
